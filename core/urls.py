c10c6be7d5c189574985e7ead93fc9e086475a50
ramyo564 <yohan032yohan@gmail.com>
2023-08-10 19:31:26 +0900

    Build: initial settings

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
+
+]
