14b5b00468d4a16ace4f3a7f8647104ddeb77187
ramyo564 <yohan032yohan@gmail.com>
2023-08-12 19:45:21 +0900

    clear git cache

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 from users import views as users_view
 from articles import views as articles_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = DefaultRouter()
 router.register(r"user", users_view.UserViewSet)
@@ -16,4 +18,4 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
