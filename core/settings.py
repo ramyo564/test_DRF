d9fa968edfd52f09a61cb89e9bdd85dd9ae71e95
ramyo564 <yohan032yohan@gmail.com>
2023-08-16 00:54:16 +0900

    change token expiration period

--- core/settings.py
+++ core/settings.py
@@ -178,7 +178,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=20),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
